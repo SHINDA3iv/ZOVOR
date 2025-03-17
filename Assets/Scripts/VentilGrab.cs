63322b600cd2c11f7ad62b4e367dee9c5cf4158f
Ivan Voitenko <vanekvote_2003@mail.ru>
2025-03-18 01:18:04 +0700

    догрузка

---
+++ Assets/Scripts/VentilGrab.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VentilGrab : XRGrabInteractable
+{
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+
+        StartCoroutine(CancleGrabWhenHandMove(args.interactorObject.transform.parent));
+    }
+
+    private IEnumerator CancleGrabWhenHandMove(Transform handTransform)
+    {
+        while (true)
+        {
+            Vector3 distance = this.transform.position - handTransform.position;
+
+            if (distance.magnitude > 0.3f)
+            {
+                this.enabled = false;
+                this.enabled = true;
+                yield break;
+            }
+            yield return null;
+        }
+    }
+}
